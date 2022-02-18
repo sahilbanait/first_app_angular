d4d53195b6ed11e0e09bf43d9bc33dcf4bdd630d
sahilbanait <77091616+sahilbanait@users.noreply.github.com>
2022-02-18 11:33:46 +0800

    First Commit

---
+++ src/app/WarningAlert/warningAlert.Component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component(
+  {
+    selector:'app-warningAlert',
+    templateUrl:'./warningAlert.component.html'
+  }
+)
+export class WarningAlertComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import  {} from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import {WarningAlertComponent} from "./WarningAlert/warningAlert.Component";
+import {SuccessAlertComponent} from "./SuccessAlert/successAlert.component";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
